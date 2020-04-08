e0b3c41c33e7efdf553e734dc98924ed4ed67299
Nicholas Keller <nic.keller328@gmail.com>
2020-04-08 15:59:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,10 +7,10 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)
-    project.add_backer(self)
-    @backed_projects << project
-  end
+  # def back_project(project)
+  #   project.add_backer(self)
+  #   @backed_projects << project
+  # end
 
 
 
